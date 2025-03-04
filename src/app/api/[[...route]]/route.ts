707056c672bcd29d6835ca5b65c1f1cdf7ee1460
PARMESH YADAV <parmesh20319@iiitd.ac.in>
2025-03-04 20:59:56 +0530

    nevermind

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -1,15 +1,5 @@
 import { httpHandler } from "@/server"
 
-// Change this from "edge" to "nodejs"
-export const runtime = "nodejs"
-
-// Add a config to specify larger size if needed
-export const config = {
-  api: {
-    bodyParser: {
-      sizeLimit: '10mb', // Adjust as needed
-    },
-  },
-}
+export const runtime = "edge"
 
 export { httpHandler as GET, httpHandler as POST }
